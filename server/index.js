76706d4437e7cb89ab884cdef9daee4bc62cb6c0
LuceroLuciano <ada_hack29@gmail.com>
2023-03-17 18:22:48 -0600

    Agregando el cors al server\index

--- server/index.js
+++ server/index.js
@@ -2,18 +2,15 @@
 const express = require("express");
 const { router } = require("./routes/index"); // importamos las rutas
 const server = express();
-//const cors = require("cors");
+const cors = require("cors");
 
-//server.use(cors());
+server.use(cors());
 
 // idicamos que las solicitudes van a traer algo en el código
 server.use(express.json());
 
 // middleware son funciones que se ejecutan antes de que se ejecute otra función
-server.use(router);
-
 server.use("/public", express.static(__dirname + "/public"));
-
 
 // exportmos la instancia del server
 module.exports = { server };
